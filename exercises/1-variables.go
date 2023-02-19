585f6278587d316fee470c196bddb48572255e97
Christian Lima <78440793+christian-devops@users.noreply.github.com>
2023-02-19 19:27:54 -0300

    ex1/variables

---
+++ exercises/1-variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//https://www.youtube.com/watch?v=5K17jFDXvWw
+	fmt.Println("Iniciando o exercício 1")
+	x := 42
+	y := "James Bond"
+	z := true
+	// One line print
+	fmt.Printf("x = %v\ny = %v\nz = %v\n", x, y, z)
+	//Multiple prints
+	fmt.Printf("x = %v\n", x)
+	fmt.Printf("y = %v\n", y)
+	fmt.Printf("z = %v\n", z)
+	fmt.Println("Fim do exercício 1")
+}
