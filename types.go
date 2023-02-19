33cfc20cb044d07fb5e7d96e7de8637da4c1082d
Christian Lima <78440793+christian-devops@users.noreply.github.com>
2023-02-19 19:19:56 -0300

    a3/types

---
+++ types.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type hotdog int
+
+var b hotdog = 10
+
+func main() {
+	x := 11
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", x)
+	//x = b -> Erro, apesar de ser derivada de int, hotdog é um tipo diferente
+	x = int(b) // Conversão de tipos -> type(value)
+	fmt.Printf("%T", x)
+	b = hotdog(x)
+}
