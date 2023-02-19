475bbd616f3b9d167499121bd6b9ebd5b9ebcf4b
Christian Lima <78440793+christian-devops@users.noreply.github.com>
2023-02-19 19:32:01 -0300

    ex2/variables-value0

---
+++ exercises/2-variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	//https://www.youtube.com/watch?v=Q7ZrIDMj9zc
+	fmt.Printf("%v\n%v\n%v\n", x, y, z) //Valor 0
+}
